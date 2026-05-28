6b41e22fae1a87fda945d28b885ddd9f818be8cc
Fenohasina08 <fenohasina.ra_fanomezana@student.passerellesnumerIques.org>
2026-05-28 22:37:12 +0300

    create ArithController

---
+++ src/main/java/com/add/demo/endpoint/rest/controller/arith/ArithController.java
@@ -0,0 +1,17 @@
+package com.add.demo.endpoint.rest.controller.arith;
+
+import com.add.demo.services.ArithService;
+import org.springframework.web.bind.annotation.GetMapping;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+public class ArithController {
+    private final ArithService arithService;
+
+    @GetMapping("/add")
+    public int add(int a, int b) {
+        return arithService.add(a, b);
+    }
+}
